deafe009f5225d7cfcfde052307abdd3eb63b91b
QuentinBotta <bottaquentwilliam@gmail.com>
2020-05-30 23:18:57 +0200

    added ex01 and ex05

---
+++ c3/ex01/ft_strncmp.c
@@ -0,0 +1,14 @@
+
+int
+	ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0' && i != n - 1)
+	{
+		i++;
+	}
+
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
